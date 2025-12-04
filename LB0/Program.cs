d1e0cba93d3692f52be793471d60bdd0958e95ab
AlexeyDesyatov <avd55@tpu.ru>
2025-12-04 09:53:33 +0700

    fix(sum):отредактирована формула

--- LB0/Program.cs
+++ LB0/Program.cs
@@ -15,13 +15,17 @@ namespace LbDesyatov1
         {
             Console.Write("Количество нечетных чисел N =");
             int N = int.Parse(Console.ReadLine());
-            int sum1 = 0;
+
+
+            var sum = 0;
+
+
             for (int i = 1; i <= N; i++)
             {
-                sum1 += i * 2 - 1;
+                sum += i + 2 - 1;
             }
             Console.Write("Сумма первых целых N нечетных чисел sum=");
-            Console.WriteLine(sum1);
+            Console.WriteLine(sum);
             Console.Read();
 
         }
